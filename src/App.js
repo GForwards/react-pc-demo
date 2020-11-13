cba8c5d89d33177e518cf48c6419bc96378e2d62
杨焱康 <704799639@qq.com>
2020-11-13 18:43:43 +0800

    增加路由

--- src/App.js
+++ src/App.js
@@ -1,29 +1,26 @@
 import React from 'react'
-import logo from './logo.svg'
-import './App.css'
-import Test from './pages/test'
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
+import Home from './pages/home'
+import Login from './pages/login'
+import Page404 from './pages/404'
 
 function App() {
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit
-          <code>src/App.js</code>
-          and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
-      <Test />
-    </div>
+    <Router>
+      <ul>
+        <li>
+          <Link to='/'>home</Link>
+        </li>
+        <li>
+          <Link to='/login'>login</Link>
+        </li>
+      </ul>
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route path='/404' component={Page404} />
+        <Route path='/login' component={Login} />
+      </Switch>
+    </Router>
   )
 }
 
---
+++ src/pages/home.js
@@ -0,0 +1,19 @@
+import React from 'react'
+// import { Link } from 'react-router-dom'
+
+const Home = () => {
+  return (
+    <div>
+      <ul>
+        {/* <li>
+          <Link to='/'>home</Link>
+        </li>
+        <li>
+          <Link to='/login'>login</Link>
+        </li> */}
+      </ul>
+    </div>
+  )
+}
+
+export default Home
